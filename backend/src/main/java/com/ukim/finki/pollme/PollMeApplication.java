f51c664915a93f33fb5dfab25410b6a4b2c32312
Igor Ognjanov <igorognjanov123@gmail.com>
2023-07-02 16:41:46 +0200

    use java instead of kotlin 2

---
+++ backend/src/main/java/com/ukim/finki/pollme/PollMeApplication.java
@@ -0,0 +1,13 @@
+package com.ukim.finki.pollme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run (PollMeApplication.class, args);
+    }
+
+}
