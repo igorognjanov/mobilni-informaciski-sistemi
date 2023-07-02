314b993cc4d580e9c0f525f973c82d6b9ab359f9
Igor Ognjanov <igorognjanov123@gmail.com>
2023-07-02 15:09:52 +0200

    init backend

---
+++ backend/src/main/kotlin/com/ukim/finki/mis/MisApplication.kt
@@ -0,0 +1,11 @@
+package com.ukim.finki.mis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MisApplication
+
+fun main(args: Array<String>) {
+    runApplication<MisApplication>(*args)
+}
