b04848761d2b512efd31b8c6bbf9a95eaeacf3a7
Angel Petrushevski <angelpetrusevski@gmail.com>
2023-08-24 23:20:35 +0200

    fixes

--- backend/src/main/java/com/ukim/finki/pollme/PollMeApplication.java
+++ backend/src/main/java/com/ukim/finki/pollme/PollMeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PollMeApplication {
-
+    
     public static void main(String[] args) {
         SpringApplication.run (PollMeApplication.class, args);
     }
